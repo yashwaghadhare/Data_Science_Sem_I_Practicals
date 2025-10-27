aba766a449a5c4153ddddb3b0321d850dc2fd9ca
Yash Waghadhare <127091514+yashwaghadhare@users.noreply.github.com>
2025-10-27 22:59:42 +0530

    Update univariate_analysis_numeric.py

--- Essential_Technologies_for_Data_Science/univariate_analysis_numeric.py
+++ Essential_Technologies_for_Data_Science/univariate_analysis_numeric.py
@@ -1,16 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load the Iris dataset
-df = pd.read_csv("Data/Iris.csv")
+df = pd.read_csv("Iris.csv")
 
-# Display basic statistics
-print("Summary statistics:")
 print(df.describe())
 
-# Univariate analysis - Histogram for SepalWidthCm
-plt.hist(df['SepalWidthCm'], edgecolor='black')
+plt.hist(df['SepalWidthCm'])
 plt.title("Distribution of Sepal Width")
 plt.xlabel("Sepal Width (cm)")
 plt.ylabel("Frequency")
-plt.show()
